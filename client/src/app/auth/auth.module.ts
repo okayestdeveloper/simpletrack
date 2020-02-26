a442e4404ab8932fda724510d79767c15f025057
Brad Ledbetter <brad.l@leantaas.com>
2020-02-26 16:10:54 -0500

    Adding my login page, setting up many styles, tweaking navbar and layout

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 
 import { AuthRoutingModule } from './auth-routing.module';
@@ -9,10 +11,16 @@ import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, LogoutComponent, AuthComponent],
+  declarations: [
+    LoginComponent,
+    LogoutComponent,
+    AuthComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,
+    MatButtonModule,
+    MatCardModule,
     AuthRoutingModule,
   ],
 })
